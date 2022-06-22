2f06d2890b8fe504b08bf30c6278be7137e5b686
Schadrack <chadrackngirimana@gmail.com>
2022-06-22 15:05:48 +0200

    setting up  backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+connectDB();
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -12,5 +17,6 @@ app.use(
     graphiql: process.env.NODE_ENV === "development",
   })
 );
+console.log(process.env.NODE_ENV);
 
 app.listen(port, console.log(`Server is running on port ${port}.`));
