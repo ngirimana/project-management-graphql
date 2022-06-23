75db0f320d2b404bc836e1bd755697ce301ed73d
Schadrack <chadrackngirimana@gmail.com>
2022-06-23 09:30:42 +0200

    setting up graphql on frontend

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Header from "./components/Header";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Hello world</h1>
+        </div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
